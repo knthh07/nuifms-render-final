41e90f15b50676fd39667402bf436a91294a75c0
DinAstral <princemamaril35@gmail.com>
2024-10-15 20:59:05 +0800

    loader

--- client/src/hooks/Loader.jsx
+++ client/src/hooks/Loader.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import Lottie from 'react-lottie';
-import animationData from '../assets/animation/loader_lottie_2.json';
+import { ClipLoader } from 'react-spinners';
 
 const Loader = ({ isLoading }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true, // Starts the animation automatically
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice', // Preserves the aspect ratio of the animation
-    },
+  const loaderContainerStyle = {
+    position: 'fixed',
+    top: '10px',
+    right: '10px',
+    zIndex: '9999',
+    width: '80px',
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   };
 
   return (
     isLoading && (
-      <div style={{ width: '80px', height: '80px' }}>
-        <Lottie options={defaultOptions} height={80} width={80} />
+      <div style={loaderContainerStyle}>
+        <ClipLoader loading={isLoading} size={50} color="#00BFFF" />
       </div>
     )
   );
