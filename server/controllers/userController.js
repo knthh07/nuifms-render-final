52bb522dd83e14bb88f69ed1efec00ca418e258a
DinAstral <princemamaril35@gmail.com>
2024-10-23 20:12:13 +0800

    asdas

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -128,6 +128,7 @@ const addUser = async (req, res) => {
         res.json({ user, email });
 
     } catch (error) {
+        console.log(error);
         return res.json({ error: 'An unexpected error occurred while creating the user' });
     }
 };
@@ -179,6 +180,7 @@ const addUserInfo = async (req, res) => {
         return res.json({ message: 'User information added successfully!', userInfo });
 
     } catch (error) {
+        console.log(error);
         return res.status(500).json({ error: 'An unexpected error occurred while creating user info' });
     }
 };
