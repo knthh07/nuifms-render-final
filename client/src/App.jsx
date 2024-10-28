3c66e738656be9468f14ad6eb73a6b10d2a8c941
DinAstral <princemamaril35@gmail.com>
2024-10-29 05:24:25 +0800

    asdsa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,9 +36,9 @@ import UserHistory from './User/UserHistory';
 import UserTrackOrder from './User/UserTrackOrder';
 
 import ProtectedRoutes from './hooks/ProtectedRoutes';
-// axios.defaults.baseURL = 'https://nuifms.onrender.com/';
+axios.defaults.baseURL = 'https://nuifms.onrender.com/';
 /* axios.defaults.baseURL = 'https://nuifms-9d4130efadd1.herokuapp.com/'; */
-axios.defaults.baseURL = 'http://localhost:5080/';
+// axios.defaults.baseURL = 'http://localhost:5080/';
 axios.defaults.withCredentials = true;
 
 function App() {
